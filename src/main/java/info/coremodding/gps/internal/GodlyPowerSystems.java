0e840a4eb7582576e464dcd96ecdfc55edf30bf5
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-03-30 09:43:47 -0700

    Moved GodlyPowerSystems to gps.internal

--- src/main/java/info/coremodding/gps/internal/GodlyPowerSystems.java
+++ src/main/java/info/coremodding/gps/internal/GodlyPowerSystems.java
@@ -1,11 +1,10 @@
-package info.coremodding.gps;
+package info.coremodding.gps.internal;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Instance;
 
 /**
  * @author James
- *         <p>
  *         The main class
  */
 @Mod(modid = "zeus_GodlyPowerSystems", name = "Godly Power Systems", version = "Alpha Dev 1")
